d0d034ff397ef8d67edfb1eb607f9b6d801b309d
Italo Pereira <italo.pereira@nodo.cc>
2018-12-11 10:54:09 -0200

    add project structure

---
+++ src/assets/js/main.js
@@ -0,0 +1,24 @@
+// Import jQuery and Plugins
+import $ from 'jquery';
+window.$ = window.jQuery = $;
+
+window.App = {
+	data: {},
+	utils: {}
+};
+
+// Components
+import Swiper from 'swiper';
+window.Swiper = Swiper;
+
+App.init = () => {
+
+	// components
+
+	// instance pages
+	App.pages = {
+	};
+
+};
+
+App.init();
