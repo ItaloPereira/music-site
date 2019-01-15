a02d42fcc8dd683606d21213ea8a11c8c39db570
Italo Pereira <italo.pereira@nodo.cc>
2019-01-15 16:05:07 -0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ git branch -D master &&
 git checkout -b master &&
 npm install && npm run build &&
 rm -rf .dist &&
-cp ./dist/** ./ &&
+cp -r dist/* ./ &&
 rm -rf src
 rm -rf node_modules
 rm -rf dist
@@ -14,3 +14,4 @@ rm package.json
 rm package-lock.json
 rm webpack.config.build.js
 rm webpack.config.dev.js
+git add --all && git commit -m 'deploy' && git push origin master
