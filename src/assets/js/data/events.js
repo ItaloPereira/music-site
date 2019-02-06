1ea45592b173f552caabda5c9b6e2d349c9eca5d
Italo Pereira <italo.pereira@nodo.cc>
2019-02-06 19:06:20 -0200

    att

--- src/assets/js/data/events.js
+++ src/assets/js/data/events.js
@@ -5,25 +5,25 @@ const Events = [
         name: 'HN Funk',
         date: '22/02',
         city: 'PELOTAS',
-        image: 'flyer2.jpg',
+        image: 'flyer1.jpg',
         link: 'https://www.facebook.com/events/210978669851748/'
     }, {
         name: 'Fp do Trem Bala',
         date: '09/03',
         city: 'PELOTAS',
-        image: 'flyer3.jpeg',
+        image: 'flyer2.jpeg',
         link: 'https://www.facebook.com/events/2146954125521997/'
     }, {
         name: 'Show do Mano Brown',
         date: '18/04',
         city: 'PELOTAS',
-        image: 'flyer4.jpeg',
+        image: 'flyer3.jpeg',
         link: 'https://www.facebook.com/events/228235331449831/'
     }, {
         name: 'Teckzada',
         date: '...',
         city: 'Jaguarão',
-        image: 'flyer5.png',
+        image: 'flyer4.png',
         link: 'javascript:;'
     }
 ]
