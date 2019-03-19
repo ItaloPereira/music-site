2f35760e00f9a2a82d8ce316a451135161955053
Italo Pereira <italo.pereira@nodo.cc>
2019-03-19 12:44:46 -0300

    update events

--- src/assets/js/data/events.js
+++ src/assets/js/data/events.js
@@ -1,29 +1,41 @@
 'use strict';
 
 const Events = [
-     {
+    {
+        name: 'Lilith Party',
+        date: '22/03',
+        city: 'PELOTAS',
+        image: 'flyer1.jpeg',
+        link: 'https://www.facebook.com/events/2200266443387009/'
+    },{
+        name: 'PVT MUSTAJ DJ',
+        date: '23/03',
+        city: 'PELOTAS',
+        image: 'flyer2.jpeg',
+        link: 'javascript:;'
+    },{
         name: 'Baile da C.A',
         date: '12/04',
         city: 'PELOTAS',
-        image: 'flyer1.jpeg',
+        image: 'flyer3.jpeg',
         link: 'https://www.facebook.com/events/786189115069052/'
     }, {
         name: 'Teckzada',
         date: '04/05',
         city: 'Jaguarão',
-        image: 'flyer2.png',
+        image: 'flyer4.png',
         link: 'javascript:;'
     }, {
         name: 'Baile da Pantera',
         date: '10/05',
         city: 'PELOTAS',
-        image: 'flyer3.jpeg',
+        image: 'flyer5.jpeg',
         link: 'https://www.facebook.com/events/352073248728148/'
     }, {
         name: 'Show do Mano Brown',
         date: '06/06',
         city: 'PELOTAS',
-        image: 'flyer4.jpeg',
+        image: 'flyer6.jpeg',
         link: 'https://www.facebook.com/events/228235331449831/'
     }
 ]
