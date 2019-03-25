5560b63dba2e3693a13494ef49fccb81f6c8293c
Italo Pereira <italo.pereira@nodo.cc>
2019-03-25 12:50:30 -0300

    update events

--- src/assets/js/data/events.js
+++ src/assets/js/data/events.js
@@ -2,23 +2,23 @@
 
 const Events = [
     {
-        name: 'Lilith Party',
-        date: '22/03',
-        city: 'PELOTAS',
-        image: 'flyer1.jpeg',
-        link: 'https://www.facebook.com/events/2200266443387009/'
-    },{
-        name: 'PVT MUSTAJ DJ',
-        date: '23/03',
-        city: 'PELOTAS',
-        image: 'flyer2.jpeg',
-        link: 'javascript:;'
-    },{
         name: 'Baile da C.A',
         date: '12/04',
         city: 'PELOTAS',
-        image: 'flyer3.jpeg',
+        image: 'flyer1.jpeg',
         link: 'https://www.facebook.com/events/786189115069052/'
+    }, {
+        name: 'Show Mc Levin',
+        date: '20/04',
+        city: 'PELOTAS',
+        image: 'flyer2.jpg',
+        link: 'https://www.facebook.com/events/357695261497120/'
+    }, {
+        name: 'Show Matuê',
+        date: '03/05',
+        city: 'Pelotas',
+        image: 'flyer4.jpeg',
+        link: 'https:3/www.facebook.com/events/287471012153597/'
     }, {
         name: 'Teckzada',
         date: '04/05',
@@ -32,10 +32,16 @@ const Events = [
         image: 'flyer5.jpeg',
         link: 'https://www.facebook.com/events/352073248728148/'
     }, {
-        name: 'Show do Mano Brown',
-        date: '06/06',
+        name: 'Baile da GR',
+        date: '08/06',
         city: 'PELOTAS',
         image: 'flyer6.jpeg',
+        link: 'javascript:;'
+    }, {
+        name: 'Show do Mano Brown',
+        date: '06/07',
+        city: 'PELOTAS',
+        image: 'flyer7.jpeg',
         link: 'https://www.facebook.com/events/228235331449831/'
     }
 ]
